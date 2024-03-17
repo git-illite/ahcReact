f5b2b775c7cc1b99ee1346a1d13e8f17fe3b9e8f
git-illite <71469298+git-illite@users.noreply.github.com>
2024-03-17 06:09:04 -0400

    adjusted programs width

--- src/Components/Programs.jsx
+++ src/Components/Programs.jsx
@@ -241,7 +241,7 @@ export default function Programs() {
                 />
               </svg>
             </ButtonBack>
-            <div className="mx-auto overflow-x-hidden overflow-y-hidden w-2/3">
+            <div className="mx-auto overflow-x-hidden overflow-y-hidden w-4/5">
               <Slider>
                 {[...reversedData].toReversed().map((item, index) => (
                   <Slide index={index} key={item.id}>
